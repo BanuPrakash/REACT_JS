80a93ef95b30a630338e12afdcd5502ec874f7ab
Banuprakash <banuprakashc@yahoo.co.in>
2026-05-12 14:17:12 +0530

    react-router-dom

--- ecomapp/src/common/NavbarComp.jsx
+++ ecomapp/src/common/NavbarComp.jsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { Link } from 'react-router-dom';
+
 
 export default function NavbarComp() {
   return (
-    <div>NavbarComp</div>
+    <Navbar bg="dark" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand as={Link} to="/">Acme</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link as={Link} to="/products">Products</Nav.Link>
+            <Nav.Link as={Link} to="/cart">Cart</Nav.Link>
+            <Nav.Link>Form</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
   )
 }
